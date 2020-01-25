921b5eceb7277ddde042999b9f789514211bf751
Mindustry <corsair.hero@gmail.com>
2020-01-24 22:59:48 -0800

    Updated README to reflect what the addons does now and how to add a timer Added comments to code

--- DiscoveryPics.lua
+++ DiscoveryPics.lua
@@ -5,14 +5,12 @@ frame:RegisterEvent("MAP_EXPLORATION_UPDATED")
 frame:RegisterEvent("UI_INFO_MESSAGE")
 frame:SetScript("OnEvent", function (self, event, ...)
 	if event == "UI_INFO_MESSAGE" then
-		if (select(2,...) == fp) then
-			Screenshot()
+		if (select(2,...) == fp) then -- take screenshot if flight path is undiscovered
+			Screenshot() -- Add C_Timer.After(duration,Screenshot) with a number in seconds for duration to set a timed screenshot
 		else
-			return nil
+			return nil 
 		end
 	else
-		Screenshot()
-	end
-
+		Screenshot() -- take screenshot when discovering new zone
 end)	
 	
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # DingPics
 
-World of Warcraft add-on to automatically screenshot when you level up.
+World of Warcraft add-on to automatically screenshot when you enter a new zone or discover a new flight path.
 
-By default the screenshot is triggered after the player enters a new zone and discovers a new flight path,
+By default the screenshot is triggered after the player enters a new zone and discovers a new flight path. Read the comments in DiscoveryPics.lua to set a timer for the screenshot.
 
 ## Install
 
